094d319f953b7be0035b64e22213af65fb44c112
Henry Mitchell <henry@henrymitchell.org>
2021-12-09 21:45:58 -0500

    Finished day 9

---
+++ day09_0.py
@@ -0,0 +1,83 @@
+"""https://adventofcode.com/2021/day/9"""
+from typing import List
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+data_dir = os.path.join(os.path.dirname(__file__), "data")
+test_dir = os.path.join(os.path.dirname(__file__), "test")
+DATA_PATH = os.path.join(data_dir, "day09_0.txt")
+TEST_PATH = os.path.join(test_dir, "day09_0.txt")
+
+
+def load_data(path: str) -> list:
+    """Load data from file"""
+    with open(path, "r") as f:
+        rows = f.read().splitlines()
+    return [list(map(int, list(row))) for row in rows]
+
+
+def one_d_minima(lst: list) -> list:
+    """Find the indices of the local minima in a list"""
+    # Need to deal with the first and last elements separately
+    out = []
+    if lst[0] < lst[1]:
+        out.append(0)
+    if lst[-1] < lst[-2]:
+        out.append(len(lst) - 1)
+    # Now the rest
+    out += [
+        i + 1 for i, (x, y, z) in enumerate(zip(lst, lst[1:], lst[2:]))
+        if y < x and y < z
+    ]
+    return out
+
+
+def row_minima(data: List[List[int]]) -> List[List[int]]:
+    """Find the indices of the local minima in each row"""
+    out = [one_d_minima(row) for row in data]
+    return out
+
+
+def column_minima(data: List[List[int]]) -> List[List[int]]:
+    """Find the indices of the local minima in each column"""
+    out = [one_d_minima(col) for col in zip(*data)]
+    return out
+
+
+def local_minima(data: List[List[int]]) -> List[int]:
+    """Find the indices of the local minima in each row and column"""
+    out = []
+    row_mins = row_minima(data)
+    col_mins = column_minima(data)
+    for y, r in enumerate(data):
+        for x, _ in enumerate(r):
+            if x in row_mins[y] and y in col_mins[x]:
+                out += [[x, y]]
+    return out
+
+
+def local_minima_values(data: List[List[int]]) -> List[int]:
+    minima = local_minima(data)
+    out = []
+    for x, y in minima:
+        out.append(data[y][x])
+    return out
+
+
+def safety(data: List[List[int]]) -> int:
+    """Find the sum of the data in the safe region"""
+    return sum(map(lambda x: x + 1, local_minima_values(data)))
+
+
+def test():
+    """Test the solution"""
+    data = load_data(TEST_PATH)
+    assert safety(data) == 15
+
+
+if __name__ == "__main__":
+    test()
+    data = load_data(DATA_PATH)
+    print(safety(data))
---
+++ day09_1.py
@@ -0,0 +1,98 @@
+"""https://adventofcode.com/2021/day/9"""
+from typing import List
+import logging
+import os
+import itertools
+import day09_0 as old
+
+logging.basicConfig(level=logging.INFO)
+
+data_dir = os.path.join(os.path.dirname(__file__), "data")
+test_dir = os.path.join(os.path.dirname(__file__), "test")
+DATA_PATH = os.path.join(data_dir, "day09_0.txt")
+TEST_PATH = os.path.join(test_dir, "day09_0.txt")
+
+
+def make_mask(data: List[List[int]]) -> List[List[int]]:
+    out = [[0 for _ in data[0]] for _ in data]
+    for y, r in enumerate(data):
+        for x, depth in enumerate(r):
+            if depth == 9:
+                out[y][x] = -1
+
+    minima = old.local_minima(data)
+    for basin_number, (x, y), in enumerate(minima):
+        out[y][x] = basin_number + 1
+
+    return out
+
+
+def adjacent_values(x: int, y: int, mask: List[List[int]]) -> List[int]:
+    x_max = len(mask[0]) - 1
+    y_max = len(mask) - 1
+
+    coordinates = set()
+
+    # This set notation looks like the dumbest thing.
+    if x != 0:
+        coordinates |= set(((x - 1, y), ))
+    if x != x_max:
+        coordinates |= set(((x + 1, y), ))
+    if y != 0:
+        coordinates |= set(((x, y - 1), ))
+    if y != y_max:
+        coordinates |= set(((x, y + 1), ))
+
+    return {mask[y_][x_] for (x_, y_) in coordinates}
+
+
+def touching_value(x: int, y: int, mask: List[List[int]], val: int) -> bool:
+    """Check if a square is touching a known value"""
+
+    return any(adj == val for adj in adjacent_values(x, y, mask))
+
+
+def all_assigned(mask: List[List[int]]) -> bool:
+    return all(mask[y][x] != 0 for x, y, in indices_iter(mask))
+
+
+def indices_iter(mask):
+    for x, y in itertools.product(range(len(mask[0])), range(len(mask))):
+        yield x, y
+
+
+def iterate(mask):
+    if all_assigned(mask):
+        return mask
+    for x, y in indices_iter(mask):
+        if mask[y][x] == 0:
+            mask[y][x] = max(adjacent_values(x, y, mask))
+    return iterate(mask)
+
+
+def basin_sizes(mask) -> List[int]:
+    numbers = list(itertools.chain(*mask))
+    n_basins = max(numbers)
+    out = []
+    for i in range(n_basins + 1):
+        out += [numbers.count(i)]
+
+    return out
+
+
+def get_value(data):
+    mask = iterate(make_mask(data))
+    sizes = list(reversed(sorted(basin_sizes(mask))))
+    out = sizes[0]*sizes[1]*sizes[2]
+    return out
+
+
+def test():
+    data = old.load_data(TEST_PATH)
+    assert get_value(data) == 1134
+
+
+if __name__ == "__main__":
+    test()
+    data = old.load_data(DATA_PATH)
+    print(get_value(data))
