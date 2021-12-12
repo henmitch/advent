f97f3804f4bcb48a2fde3aa6b908bad4f6f6fd8a
Henry Mitchell <henry@henrymitchell.org>
2021-12-11 19:08:29 -0500

    Started on 2020 (days 1-3)

---
+++ 2020/code/day01_0.py
@@ -0,0 +1,31 @@
+"""https://adventofcode.com/2020/day/1"""
+import os
+from typing import List
+
+import boilerplate as bp
+
+DATA_PATH = os.path.join(bp.data_dir, "day01.txt")
+TEST_PATH = os.path.join(bp.test_dir, "day01.txt")
+
+
+def load_data(path):
+    """Load data from file"""
+    with open(path, "r") as f:
+        return list(map(int, f.read().splitlines()))
+
+
+def find_pair_product(data: List[int]) -> int:
+    for i, line in enumerate(data):
+        if (remainder := 2020 - line) in data[i + 1:]:
+            return line*remainder
+
+
+def test():
+    test_data = load_data(TEST_PATH)
+    assert find_pair_product(test_data) == 514579
+
+
+if __name__ == "__main__":
+    test()
+    data = load_data(DATA_PATH)
+    print(find_pair_product(data))
