c6b2b373e375303cae321a514c726f4cadc647bf
Henry Mitchell <henry@henrymitchell.org>
2021-12-13 07:50:42 -0500

    Finished day 13

---
+++ 2021/code/day13_0.py
@@ -0,0 +1,91 @@
+"""https://adventofcode.com/2021/day/13"""
+import os
+from typing import List, Set, Tuple
+
+import boilerplate as bp
+
+DATA_PATH = os.path.join(bp.data_dir, "day13.txt")
+TEST_PATH = os.path.join(bp.test_dir, "day13.txt")
+
+
+def load_data(path):
+    with open(path, "r") as f:
+        input_ = f.read()
+
+    points_raw, folds_raw = input_.split("\n\n")
+
+    points_raw = points_raw.split("\n")
+    points = set(tuple(map(int, point.split(","))) for point in points_raw)
+
+    folds = []
+    for fold_raw in folds_raw.split("\n"):
+        direction, location = fold_raw.split("=")
+        folds.append((direction[-1], int(location)))
+
+    return points, folds
+
+
+def fold_up(point: Tuple[int], fold_: int) -> Set[tuple]:
+    if fold_ > point[1]:
+        return {
+            point,
+        }
+
+    return {
+        (point[0], fold_ - (point[1] - fold_)),
+    }
+
+
+def fold_left(point: Tuple[int], fold_: Tuple[int]) -> Set[tuple]:
+    if fold_ > point[0]:
+        return {
+            point,
+        }
+
+    return {
+        (fold_ - (point[0] - fold_), point[1]),
+    }
+
+
+def fold(points: Set[Tuple[int]], folds: List[Tuple[int]]) -> Set[Tuple[int]]:
+    out = set()
+    direction, location = folds[0]
+    for point_x, point_y in points:
+        if direction == "x":
+            out |= fold_left((point_x, point_y), location)
+        else:
+            out |= fold_up((point_x, point_y), location)
+    if len(folds) > 1:
+        return fold(out, folds[1:])
+    return out
+
+
+def count_dots_after_fold(points, folds):
+    return len(fold(points, folds))
+
+
+def pretty_print(points: Set[Tuple[int]]):
+    x_max = max(x for x, _ in points)
+    y_max = max(y for _, y in points)
+    out = [["." for _ in range(x_max + 1)] for __ in range(y_max + 1)]
+    for point in points:
+        out[point[1]][point[0]] = "#"
+    return "\n".join("".join(row) for row in out) + "\n"
+
+
+def test():
+    points, folds = load_data(TEST_PATH)
+    print(pretty_print(points))
+    print(pretty_print(fold(points, [folds[0]])))
+    print(pretty_print(fold(points, folds)))
+    assert count_dots_after_fold(points, [folds[0]]) == 17
+
+
+def main():
+    points, folds = load_data(DATA_PATH)
+    print(count_dots_after_fold(points, [folds[0]]))
+
+
+if __name__ == "__main__":
+    test()
+    main()
