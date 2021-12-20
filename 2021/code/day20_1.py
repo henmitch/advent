5d792780691e44c20f44f5aa2267fb184f7d9a0c
Henry Mitchell <henry@henrymitchell.org>
2021-12-20 02:01:16 -0500

    Made day 20 run time ~0.5x

--- 2021/code/day20_1.py
+++ 2021/code/day20_1.py
@@ -2,6 +2,7 @@
 from itertools import cycle
 import day20_0 as old
 
+
 def enhance_a_lot(img: old.Image, alg: str, n_times: int = 50) -> old.Image:
     padder = "."
     for _ in range(n_times):
@@ -9,14 +10,15 @@ def enhance_a_lot(img: old.Image, alg: str, n_times: int = 50) -> old.Image:
         padder = old.enhance([[padder]], alg, padder)[0][0]
     return img
 
+
 def test():
     data = old.load_data(old.TEST_PATH)
-    assert old.n_illuminated(enhance_a_lot(*data)) == 3351
+    enhanced = enhance_a_lot(*data)
+    assert old.n_illuminated(enhanced) == 3351
+
 
 if __name__ == "__main__":
     test()
     data = old.load_data(old.DATA_PATH)
-    assert old.n_illuminated(enhance_a_lot(*data, 2)) == 5354
     enhanced = enhance_a_lot(*data)
-    old.pretty_print(enhanced)
     print(old.n_illuminated(enhanced))
