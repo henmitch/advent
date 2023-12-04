d42a356c1c50df1084d7ce2fe0c7c851ad091112
Henry Mitchell <henry@henrymitchell.org>
2023-12-04 06:31:48 -0500

    Finished 2023 day 4

---
+++ 2023/code/day04_0.py
@@ -0,0 +1,61 @@
+"""https://adventofcode.com/2023/day/3"""
+import boilerplate as bp
+
+TEST_PATH = bp.get_test_path()
+DATA_PATH = bp.get_data_path()
+
+
+def parse(line: str) -> tuple[int, list[int], list[int]]:
+    id_, all_nums = line.split(": ")
+    wins, nums = all_nums.split(" | ")
+    id_ = int(id_.split()[-1])
+    wins = list(map(int, wins.split()))
+    nums = list(map(int, nums.split()))
+    return id_, wins, nums
+
+
+class Card:
+
+    def __init__(self, id_: int, wins: list[int], nums: list[int]) -> None:
+        self.id_ = id_
+        self.wins = wins
+        self.nums = nums
+
+    def n_matches(self):
+        return len(set(self.wins) & set(self.nums))
+
+    def score(self):
+        out = 0
+        for num in self.nums:
+            if num not in self.wins:
+                continue
+            if not out:
+                out = 1
+            else:
+                out *= 2
+        return out
+
+
+def load_data(path: str) -> list[Card]:
+    with open(path, "r") as f:
+        raw = f.read().splitlines()
+    return [Card(*parse(line)) for line in raw]
+
+
+def run(data: list[Card]) -> int:
+    return sum(card.score() for card in data)
+
+
+def test():
+    data = load_data(TEST_PATH)
+    assert run(data) == 13
+
+
+def main():
+    data = load_data(DATA_PATH)
+    print(run(data))
+
+
+if __name__ == "__main__":
+    test()
+    main()
---
+++ 2023/code/day04_1.py
@@ -0,0 +1,26 @@
+"""https://adventofcode.com/2023/day/3"""
+from day04_0 import DATA_PATH, TEST_PATH, Card, load_data
+
+
+def run(cards: list[Card]) -> int:
+    n_copies = {card.id_: 1 for card in cards}
+    for card in cards:
+        endpoint = min(card.id_ + card.n_matches(), len(cards)) + 1
+        for id_ in range(card.id_ + 1, endpoint):
+            n_copies[id_] += n_copies[card.id_]
+    return sum(n_copies.values())
+
+
+def test():
+    data = load_data(TEST_PATH)
+    assert run(data) == 30
+
+
+def main():
+    data = load_data(DATA_PATH)
+    print(run(data))
+
+
+if __name__ == "__main__":
+    test()
+    main()
