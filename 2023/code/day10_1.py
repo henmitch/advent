268f3eb91398b44ce538a7c3478de5f03aae0ab0
Henry Mitchell <henry@henrymitchell.org>
2023-12-10 10:41:22 -0500

    Finished 2023 day 10
    Still very slow (~11 seconds)

--- 2023/code/day10_1.py
+++ 2023/code/day10_1.py
@@ -1,6 +1,7 @@
 """https://adventofcode.com/2022/day/10"""
 import itertools
 from collections import deque
+from typing import Any
 
 import boilerplate as bp
 from day10_0 import DATA_PATH, DIRS, PIPES, Pipes, load_data
@@ -24,14 +25,14 @@ SEPARATES = {
 }
 
 
-def get_other(s: str, groups: tuple[tuple, tuple]) -> tuple:
+def get_other(s: Any, groups: tuple[tuple, tuple]) -> tuple:
     for group in groups:
         if s not in group:
             return group
     raise ValueError(f"{s} not found in {groups}")
 
 
-def get_group(s: str, groups: tuple[tuple, tuple]) -> tuple:
+def get_group(s: Any, groups: tuple[tuple, tuple]) -> tuple:
     for group in groups:
         if s in group:
             return group
@@ -45,6 +46,45 @@ def is_integral(c: complex) -> bool:
 def run(data: Pipes) -> int:
     pipes = Pipes(data)
     bounds = pipes.walk()
+
+    # Walking along loop
+    loc = bounds[1]
+    pipe = pipes[loc]
+    add_to_left, add_to_right = SEPARATES[pipe]
+    left = {loc + ALL_DIRS[l] for l in add_to_left if l not in bounds}
+    left |= {loc + ALL_DIRS[l]/2 for l in add_to_left}
+    right = {loc + ALL_DIRS[r] for r in add_to_right if r not in bounds}
+    right |= {loc + ALL_DIRS[r]/2 for r in add_to_right}
+
+    for loc in bounds[2:]:
+        pipe = pipes[loc]
+        for dir_, step in ALL_DIRS.items():
+            if dir_ in PIPES[pipe]:
+                continue
+            if loc + step in bounds:
+                step /= 2
+            next_point = loc + step
+            if next_point in left:
+                add_to_left = get_group(dir_, SEPARATES[pipe])
+                add_to_right = get_other(dir_, SEPARATES[pipe])
+                break
+            if next_point in right:
+                add_to_left = get_other(dir_, SEPARATES[pipe])
+                add_to_right = get_group(dir_, SEPARATES[pipe])
+                break
+        else:
+            print(f"Didn't find a place for {loc}")
+        for l in add_to_left:
+            to_add = loc + ALL_DIRS[l]
+            if to_add not in bounds:
+                left.add(to_add)
+            left.add(to_add - ALL_DIRS[l]/2)
+        for r in add_to_right:
+            to_add = loc + ALL_DIRS[r]
+            if to_add not in bounds:
+                right.add(to_add)
+            right.add(to_add - ALL_DIRS[r]/2)
+
     frame = ([-1 + y*1j for y in range(pipes.height)] +
              [pipes.width + y*1j for y in range(pipes.height)] +
              [x - 1j for x in range(pipes.width)] +
@@ -58,6 +98,10 @@ def run(data: Pipes) -> int:
         connected_to_edge.add(current)
         for step in ADJACENTS:
             next_ = current + step
+            for side in (left, right):
+                if next_ in side:
+                    inside = get_other(side, ((left, ), (right, )))[0]
+                    break
             if pipes.oob(next_):
                 continue
             if next_ in bounds:
@@ -69,66 +113,35 @@ def run(data: Pipes) -> int:
 
             to_check.append(next_)
 
-    # print(pretty_print(pipes, bounds, set(), connected_to_edge))
+    inside = {v for v in inside if is_integral(v)} - set(bounds)
 
-    # Walking along loop
-    known_inside = set()
-    known_outside = connected_to_edge
-    to_check = deque(bounds)
+    # Now, we go through everybody on the inside
+    to_check = deque(inside)
     while to_check:
-        loc = to_check.popleft()
-        pipe = pipes[loc]
-        if pipe == "S":
-            continue
-        # Check if any non-pipe neighbors are known
-        for dir_, step in ALL_DIRS.items():
-            if dir_ in PIPES[pipe]:
-                continue
-            if loc + step in bounds:
-                step /= 2
-            if loc + step in known_outside:
-                add_to_in = get_other(dir_, SEPARATES[pipe])
-                add_to_out = get_group(dir_, SEPARATES[pipe])
-                break
-            if loc + step in known_inside:
-                add_to_in = get_group(dir_, SEPARATES[pipe])
-                add_to_out = get_other(dir_, SEPARATES[pipe])
-                break
-        else:
-            if loc not in to_check and loc not in bounds:
-                to_check.append(loc)
-            continue
-        for in_ in add_to_in:
-            if pipes.oob(loc + ALL_DIRS[in_]):
-                continue
-            known_inside.add(loc + ALL_DIRS[in_])
-            known_inside.add(loc + ALL_DIRS[in_]/2)
-        for out in add_to_out:
-            # if pipes.oob(loc + ALL_DIRS[out]):
-            #     continue
-            known_outside.add(loc + ALL_DIRS[out])
-            known_outside.add(loc + ALL_DIRS[out]/2)
+        current = to_check.popleft()
+        for step in ADJACENTS:
+            if (next_ := current + step) not in bounds and next_ not in inside:
+                inside.add(next_)
+                to_check.append(next_)
 
-    known_inside = {v for v in known_inside if is_integral(v)} - set(bounds)
-    print(pretty_print(pipes, bounds, known_inside, known_outside))
-    return len(known_inside)
+    print(pretty_print(pipes, bounds, right, left))
+    return len(inside)
 
 
-def pretty_print(pipes: Pipes, bounds: list[complex],
-                 known_inside: set[complex],
-                 known_outside: set[complex]) -> str:
+def pretty_print(pipes: Pipes, bounds: list[complex], inside: set[complex],
+                 outside: set[complex]) -> str:
     out = ""
     for y, line in enumerate(pipes.data):
         for x, char in enumerate(line):
             loc = x + y*1j
             if loc in bounds:
                 out += char
-            elif loc in known_inside:
+            elif loc in inside:
                 out += "I"
-            elif loc in known_outside:
+            elif loc in outside:
                 out += "O"
             else:
-                out += char
+                out += " "
         out += "\n"
     return out
 
