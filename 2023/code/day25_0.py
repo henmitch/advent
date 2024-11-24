a26aae79dd444cb3117c1a240e59b3b6d5e94394
Henry Mitchell <henry@henrymitchell.org>
2024-11-24 11:01:09 -0500

    Finished 2023

--- 2023/code/day25_0.py
+++ 2023/code/day25_0.py
@@ -1,6 +1,6 @@
 """https://adventofcode.com/2023/day/24"""
 from __future__ import annotations
-from heapq import heappop, heappush
+from random import choice
 
 import boilerplate as bp
 
@@ -10,46 +10,46 @@ DATA_PATH = bp.get_data_path()
 
 class Network:
 
-    def __init__(self, edges: dict[str, frozenset[str]]) -> None:
+    def __init__(self,
+                 edges: dict[str, tuple[str, ...]],
+                 sizes: dict[str, int] = None) -> None:
         self.edges = edges
-        self.nodes = frozenset(edges.keys())
-
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, Network):
-            return False
-        return self.edges == other.edges
-
-    def without(self, nodes: set[str]) -> Network:
-        """Return a copy of the network without the given nodes"""
-        out = {}
-        for node in self.nodes:
-            if node not in nodes:
-                out[node] = self.edges[node] - nodes
-        return Network(out)
-
-    def subgraph(self, nodes: set[str]) -> Network:
-        """Return a subgraph of the network with only the given nodes"""
-        out = {}
-        for node in self.nodes:
-            if node in nodes:
-                out[node] = self.edges[node] & nodes
-        return Network(out)
-
-    def neighbors_of(self, node: str) -> set[str]:
-        return self.edges[node]
-
-    def subgraph_neighbors(self, nodes: set[str]) -> set[str]:
-        """The neighbors of a subgraph, excluding itself"""
-        out = set()
-        for node in nodes:
-            out |= self.neighbors_of(node)
-        return out - nodes
+        self.nodes = tuple(edges.keys())
+        if sizes is None:
+            sizes = {node: 1 for node in self.nodes}
+        self.sizes = sizes
 
     def edge_count(self) -> int:
         return sum(len(self.edges[node]) for node in self.edges)//2
 
     def most_connected_node(self) -> str:
         return max(self.nodes, key=lambda x: len(self.edges[x]))
+
+    def combine(self, a: str, b: str) -> Network:
+        """Combine two nodes into one"""
+        new_edges = {}
+        new_edges[a] = self.edges[a] + self.edges[b]
+        new_edges[a] = [x for x in new_edges[a] if x not in (a, b)]
+
+        new_sizes = self.sizes.copy()
+        new_sizes[a] = self.sizes[a] + self.sizes[b]
+        new_sizes.pop(b)
+
+        for node in self.nodes:
+            if node not in (a, b):
+                new_edges[node] = [
+                    x if x not in (a, b) else a for x in self.edges[node]
+                ]
+        return Network(new_edges, new_sizes)
+
+    def random_edge(self) -> str:
+        # Not actually random, but close enough
+        random_node = choice(self.nodes)
+        other = choice(self.edges[random_node])
+        return random_node, other
+
+    def copy(self) -> Network:
+        return Network(self.edges.copy())
 
 
 def load_data(path: str) -> Network:
@@ -58,8 +58,8 @@ def load_data(path: str) -> Network:
     edges = {}
     for line in raw:
         for a, b in _parse(line):
-            edges[a] = edges.get(a, set()) | {b}
-            edges[b] = edges.get(b, set()) | {a}
+            edges[a] = edges.get(a, []) + [b]
+            edges[b] = edges.get(b, []) + [a]
     return Network(edges)
 
 
@@ -72,33 +72,15 @@ def _parse(line: str) -> list[tuple[str, str]]:
 
 
 def run(data: Network) -> int:
-    # Total number of edges
-    number_of_edges = data.edge_count()
-    # Start with a random node (the in-subgraph)
-    to_check = []
-    seen = set()
-    heappush(to_check, frozenset({data.most_connected_node()}))
-    n = 0
-    while to_check:
-        n += 1
-        if not n%1000:
-            print(n, len(to_check), len(seen))
-        ins_nodes = heappop(to_check)
-        ins = data.subgraph(ins_nodes)
-        outs = data.without(ins_nodes)
-        # Count the number of connections to the out-subgraph
-        if number_of_edges - (ins.edge_count() + outs.edge_count()) == 3:
-            # If we have 3, that's our split
-            print(f"{n=}")
-            return len(ins.nodes)*len(outs.nodes)
-        # If it's more than 3, look at all the subgraphs created by adding
-        # each of the neighbors of the in-subgraph to the in-subgraph
-        for new_node in data.subgraph_neighbors(ins_nodes):
-            new_graph = frozenset(ins_nodes | {new_node})
-            if new_graph not in seen:
-                heappush(to_check, new_graph)
-                seen.add(new_graph)
-    raise ValueError("Couldn't find the split!")
+    while True:
+        checking = data.copy()
+        # Randomly contract the graph until two nodes remain
+        while len(checking.nodes) > 2:
+            a, b = checking.random_edge()
+            checking = checking.combine(a, b)
+        if checking.edge_count() == 3:
+            break
+    return checking.sizes[checking.nodes[0]]*checking.sizes[checking.nodes[1]]
 
 
 def test():
