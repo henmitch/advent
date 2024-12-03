e3e8762017ab52563028415f501e9d586ad82f10
Henry Mitchell <henry@henrymitchell.org>
2024-12-03 05:45:59 -0500

    Added answer storage

--- 2024/code/boilerplate.py
+++ 2024/code/boilerplate.py
@@ -1,6 +1,7 @@
 import os
 import re
 import sys
+from typing import Any
 
 import requests
 from dotenv import load_dotenv
@@ -12,15 +13,33 @@ _url = "https://adventofcode.com/2024/day/{}/input"
 
 data_dir = os.path.join(os.path.dirname(__file__), os.pardir, "data")
 test_dir = os.path.join(os.path.dirname(__file__), os.pardir, "test")
+answers_dir = os.path.join(os.path.dirname(__file__), os.pardir, "answers")
 
 
-def file_name(suffix: str = None) -> str:
+class colors:
+    """ANSI color codes for terminal output."""
+    RED = "\033[91m"
+    YELLOW = "\033[93m"
+    GREEN = "\033[92m"
+    BLUE = "\033[94m"
+    CYAN = "\033[96m"
+    MAGENTA = "\033[95m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
+
+
+def file_name(suffix: str = None, keep_part: bool = False) -> str:
     caller = os.path.split(sys.argv[0])[1]
     if suffix is not None:
         suffix = "_" + suffix
     else:
         suffix = ""
-    return re.sub(r"_[0-9]+\.py", f"{suffix}.txt", caller)
+    if keep_part:
+        replace = r".py"
+    else:
+        replace = r"_[0-9]+\.py"
+    return re.sub(replace, f"{suffix}.txt", caller)
 
 
 def day_num() -> str:
@@ -48,3 +67,9 @@ def get_data_path(suffix: str = None) -> str:
 
 def get_test_path(suffix: str = None) -> str:
     return os.path.join(test_dir, file_name(suffix))
+
+
+def write_answer(answer: Any) -> None:
+    file = os.path.join(answers_dir, file_name(keep_part=True))
+    with open(file, "a") as f:
+        f.write(str(answer) + "\n")
