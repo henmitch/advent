8710fe5e0c1e1ad160b88bc755751e4f47243ba0
Henry Mitchell <henry@henrymitchell.org>
2024-12-08 15:58:53 -0500

    Fixed 2024 day 8 part 2

--- 2024/code/day08_1.py
+++ 2024/code/day08_1.py
@@ -1,6 +1,5 @@
 """https://adventofcode.com/2024/day/8"""
 import itertools
-from math import gcd
 
 import boilerplate as bp
 from day08_0 import DATA_PATH, TEST_PATH, is_oob, load_data
@@ -8,7 +7,6 @@ from day08_0 import DATA_PATH, TEST_PATH, is_oob, load_data
 
 def antinodes(a: complex, b: complex, bound: tuple[int, int]) -> set[complex]:
     slope = b - a
-    slope /= gcd(int(slope.real), int(slope.imag))
     out = set()
     # There's definitely a more efficient way to do this, but whatever
     adding = a
