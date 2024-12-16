66ab97f72176350358e70c09529ca571a578a97b
Henry Mitchell <henry@henrymitchell.org>
2024-12-16 18:24:14 -0500

    Another impossibly slow 2024 day 16 part 2
    Gonna find connections between road forks

--- 2024/code/day16_1.py
+++ 2024/code/day16_1.py
@@ -10,22 +10,67 @@ from day16_0 import DATA_PATH, TEST_PATH_0, TEST_PATH_1, Grid, State, manhattan
 
 class GridWithPaths(Grid):
 
-    def is_corner(self, loc: complex) -> bool:
-        return (not (loc + 1j in self.walls and loc - 1j in self.walls)
-                or not (loc + 1 in self.walls and loc - 1 in self.walls))
+    def __iter__(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                yield x + y*1j
+
+    def neighbors(self, loc: complex) -> list[complex]:
+        return [loc + d for d in [1, -1, 1j, -1j] if loc + d not in self.walls]
+
+    def is_dead_end(self, loc: complex) -> bool:
+        if loc in self.walls or loc == self.start or loc == self.end:
+            return False
+        return len(self.neighbors(loc)) == 1
+
+    def is_fork(self, loc: complex) -> bool:
+        if loc in self.walls:
+            return False
+        return len(self.neighbors(loc)) > 2
+
+    def is_bend(self, loc: complex) -> bool:
+        neighbors = self.neighbors(loc)
+        return len(neighbors) >= 2 and sum(neighbors) != 0
+
+    def find_dead_ends(self) -> set[complex]:
+        out = set()
+        for loc in self:
+            if loc in out or not self.is_dead_end(loc):
+                continue
+            out.add(loc)
+            n = loc
+            while not self.is_fork(n) and n not in [self.start, self.end]:
+                out.add(n)
+                n = (set(self.neighbors(n)) - out).pop()
+        print(f"Found {len(out)} dead ends")
+        return out
+
+    def find_score(self, path: list[complex]) -> dict[complex, int]:
+        out = {loc: 0 for loc in path + [self.start - 1]}
+        direction = 1
+        for a, b in zip(path, path[1:]):
+            if b - a != direction:
+                # We've turned
+                out[b] = out[a] + 1001
+                direction = b - a
+            else:
+                out[b] = out[a] + 1
+        return out
 
     def estimated_distance(self, loc: complex) -> int:
         turns = int(loc.real != self.end.real or loc.imag != self.end.imag)
         return manhattan(loc, self.end) + 1000*turns
 
     def find_paths(self) -> list[list[complex]]:
-        end_score, _ = self.walk()
+        do_not_enter = self.find_dead_ends() | set(self.walls)
+        end_score, example = self.walk()
+        print(f"End score: {end_score}")
         out = []
         q = [
             State(manhattan(self.start, self.end), 0, self.start,
                   [self.start - 1, self.start])
         ]
-        min_scores = {self.start: 0}
+        min_scores = self.find_score(example)
 
         while q:
             score, loc, path = heapq.heappop(q)
@@ -43,14 +88,14 @@ class GridWithPaths(Grid):
                 if new_loc in path[1:-1]:
                     continue
 
-                if new_loc in self.walls:
+                if new_loc in do_not_enter:
                     continue
 
                 if new_score > end_score:
                     continue
 
                 if (new_score > min_scores.get(new_loc, inf)
-                        and not self.is_corner(new_loc)):
+                        and not self.is_bend(new_loc)):
                     continue
 
                 min_scores[new_loc] = new_score
